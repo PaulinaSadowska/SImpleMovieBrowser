8e132eb4254245744d4548436872dc189b6e453b
Paulina Sadowska <paulina.sadowska@student.put.poznan.pl>
2017-03-25 14:56:04 +0100

    empty activity

---
+++ app/src/main/java/com/nekodev/paulina/sadowska/simplemoviebrowser/MovieListActivity.java
@@ -0,0 +1,15 @@
+package com.nekodev.paulina.sadowska.simplemoviebrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MovieListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_list);
+    }
+}
